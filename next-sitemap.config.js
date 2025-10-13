e0c9eb4e44824aeec4e3e7a33e6f1de73d0a37a4
laptech88 <laptech88@gmail.com>
2025-10-13 19:12:54 +0500

    Added Sitemap Url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://localhost:3000',
+  siteUrl: process.env.SITE_URL || 'https://j7group.com.pk',
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: 'daily',
